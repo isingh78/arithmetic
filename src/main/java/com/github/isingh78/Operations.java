26d3910b5d9baeedda72b4d7e683667b145b0725
Randhawa <Inderpal.Randhawa@aristocrat.com>
2020-07-06 16:53:31 +0530

    added arthmetic operations

---
+++ src/main/java/com/github/isingh78/Operations.java
@@ -0,0 +1,20 @@
+package com.github.isingh78;
+
+public class Operations {
+
+    public static int AddNumber(int i,int j)
+    {
+        return i+j;
+    }
+
+    public static int MultiplyNumber(int i,int j)
+    {
+        return i*j;
+    }
+
+    public static int SubtractNumber(int i,int j)
+    {
+        return i-j;
+    }
+
+}
